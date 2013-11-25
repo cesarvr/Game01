bf687c92024f80bfa311e04384b658ec054164a8
kobe84 <cesarv01@gmail.com>
2013-11-25 19:49:19 +0100

    Initial Commit

---
+++ Game01/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Game01
+//
+//  Created by Cesar Luis Valdez on 25/11/13.
+//  Copyright (c) 2013 Cesar Luis Valdez. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
